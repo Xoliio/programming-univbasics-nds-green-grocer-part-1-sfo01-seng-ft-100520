c3a6fe892755b0eb2b70248c51af19a26ee4a589
Inee Ader <xoliio@gmail.com>
2020-09-29 17:21:43 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -12,30 +12,9 @@ end
 def consolidate_cart(cart)
   tidy_cart = []
   cart_index = 0
-  new_item = find_item_by_name_in_collection(cart[cart_index][:item], tidy_cart)
+
   cart.each do |hash|
-    tidy_cart_index = 0
-    tidy_cart.each do |item_to_add|
-      if item_to_add[:item] == new_item[:item]
-        item_to_add[:count] += 1
-      end
-      tidy_cart_index +=1
-    end
-    binding.pry
-  end
-    else
-      new_item = {
-        :item => cart[index][:item],
-        :price => cart[index][:price],
-        :clearance => cart[index][:clearance],
-        :count => 1
-      }
-      tidy_cart << new_item
-    end
-    index += 1
-  end
-  tidy_cart
-end
+  
 
   # while index < cart.length
   #   new_item = find_item_by_name_in_collection(cart[index][:item], tidy_cart)
